e2afe035944ec42ffff25465a315c518ca050558
alakambhavya <alakambhavya@gmail.com>
2026-06-02 23:33:35 +0530

    update

--- pattern_pgm.py
+++ pattern_pgm.py
@@ -160,3 +160,15 @@ for row in range(7):
 			print(" ",end=" ")	
 	print()
 
+print("___________________________________")
+
+for row in range(7):
+	for col in range(4):
+		if col==0 or col==3 or row==3 :
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+
+
