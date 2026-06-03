e306a9647ae0adff946fea2ce984707016d264c2
alakambhavya <alakambhavya@gmail.com>
2026-06-03 09:33:33 +0530

    pattern programs

--- pattern_pgm.py
+++ pattern_pgm.py
@@ -91,16 +91,6 @@ for row in range(9):
 		print("*",end=" ")
 	print()
 
-print("__________________________")
-
-for row in range(4):
-	for col in range(7):
-		if col-row==0 or col+row==6:
-			print("*",end=" ")
-		else:
-			print(" ",end=" ")
-	print()
-
 print("______________________________________")
 
 for row in range(8):
@@ -169,6 +159,219 @@ for row in range(7):
 		else:
 			print(" ",end=" ")	
 	print()
+
+print("______________________________")
+
+for row in range(6):
+	for col in range(5):
+		if col==0 or row==0 or row==3:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("______________________")
+
+
+for row in range(7):
+	for col in range(6):
+		if col>=1 and col<=3 and row==0 or  col>=1 and col<=3 and row==4 or row>=1 and row<=3 and col==0 or col>=2 and col<=5 and row==2 or row>=3 and row<=6 and col==5 :
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("_____________________________")
+
+           
+for row in range(7):
+	for col in range(4):
+		if col==0 or col==3 or row==3 :
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("__________________________")
+
+for row in range(7):
+	for col in range(5):
+		if col==2 or row==0 or row==6:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("__________________________")
+
+for row in range(7):
+	for col in range(5):
+		if col==2 or row==0 or row>=5 and row<=6 and col==0 or row==6 and col==1:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("__________________________")
+
+for row in range(7):
+	for col in range(5):
+		if col==0 or col+row==3 or row-col==3:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("__________________________")
+
+for row in range(7):
+	for col in range(5):
+		if col==0 or row==6:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("__________________________")
+
+for row in range(6):
+	for col in range(5):
+		if col==0 or col==4 or  col==1 and row==1 or row==2 and col==2 or col>=3 and col<=3 and row==1:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("__________________________")
+
+for row in range(7):
+	for col in range(7):
+		if col==0 or col==6 or col-row==0:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("______________________________")
+
+for row in range(6):
+	for col in range(6):
+		if col>=1 and col<=4 and row==0 or col>=1 and col<=4 and row==5 or row>=1 and row<=4 and col==0 or row>=1 and row<=4 and col==5:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("_______________________________")
+
+for row in range(8):
+	for col in range(5):
+		if col==0 or col>=0 and col<=3 and row==0 or col>=0 and col<=3 and row==3 or row>=1 and row<=2 and col==4:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("______________________________")
+
+for row in range(7):
+	for col in range(7):
+		if col>=1 and col<=4 and row==0 or col>=1 and col<=4 and row==5 or row>=1 and row<=4 and col==0 or row>=1 and row<=4 and col==5 or row==6 and col==6 or  row==5 and col==5 or  row==4 and col==4 or row==3 and col==3:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("___________________________________")
+
+for row in range(8):
+	for col in range(5):
+		if col==0 or col>=0 and col<=3 and row==0 or col>=0 and col<=3 and row==3 or row>=1 and row<=2 and col==4 or row-col==3:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("_____________________________________")
+
+for row in range(9):
+	for col in range(5):
+		if col>=1 and col<=4 and row==0 or col>=1 and col<=3 and row==4 or col>=0 and col<=3 and row==8 or row>=1 and row<=3 and col==0 or  row>=5 and row<=7 and col==4 :
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("_________________________________________")
+ 
+for row in range(5):
+	for col in range(7):
+		if col==3 or row==0:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("_________________________________________")
+ 
+for row in range(6):
+	for col in range(5):
+		if row>=0 and row<=4 and col==0 or row>=0 and row<=4 and col==4 or col>=1 and col<=3 and row==5:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")	
+	print()
+
+print("___________________________")
+
+for row in range(4):
+	for col in range(7):
+		if col-row==0 or col+row==6:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")
+	print()
+
+print("____________________")
+
+for row in range(6):
+	for col in range(5):
+		if col==0 or col==4 or col==1 and row==4 or col==2 and row==3 or col==3 and row==4:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")
+	print()
+
+print("____________________________________")
+
+for row in range(5):
+	for col in range(5):
+		if col+row==4 or col-row==0:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")
+	print()
+
+print("____________________________________")
+
+for row in range(5):
+	for col in range(5):
+		if col+row==4 or col==0 and row==0 or col==1 and row==1:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")
+	print()
+
+print("____________________________________")
+
+for row in range(5):
+	for col in range(5):
+		if row==0 or row==4 or row+col==4:
+			print("*",end=" ")
+		else:
+			print(" ",end=" ")
+	print()
+
 
 
 
